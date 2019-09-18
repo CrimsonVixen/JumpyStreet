054c964f7aea065f7d8314e5da922e1c6de88f3b
Steven <superstevo96@gmail.com>
2019-09-18 15:46:54 -0400

    Menu Creation
    Added menu scene, help scene, and game scene, along with all assets needed to start them

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+    public void OnStartButtonClick()
+    {
+        //Change scene name to whatever the completed game scene is
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnHelpButtonClick()
+    {
+        SceneManager.LoadScene("HelpScene");
+    }
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnMenuButtonClick()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
