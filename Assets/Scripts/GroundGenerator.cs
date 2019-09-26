787deb7418625bcf0d8bbdb4fe437f5bbf22fe57
atjones1 <55410868+atjones1@users.noreply.github.com>
2019-09-25 23:15:49 -0400

    Started basic ground generation
    Basic ground generation, added light and dark variants of grass, water, and roads (will work on adding lines to the roads later).

---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    public GameObject[] grounds;
+    public GameObject groundParent;
+
+    private int groundsToGenerate = 20;
+    private int groundPosition = 6;
+
+    public void Start()
+    {
+        for(int i = 0; i < groundsToGenerate; i++)
+        {
+            GenerateGround();
+        }
+    }
+
+    public void GenerateGround()
+    {
+        GameObject tempGO = grounds[Random.Range(0, grounds.Length - 1)];
+        Instantiate(tempGO, new Vector3(tempGO.transform.position.x, tempGO.transform.position.y, groundPosition), Quaternion.identity, groundParent.transform);
+        groundPosition++;
+    }
+}
