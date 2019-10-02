57477802253730da50892bf676ecaa355c9fb6af
CrimsonVixen <JennJP65@yahoo.com>
2019-10-02 16:38:12 -0400

    Player Animation Setup
    Set up the base horizontal animation movement stuff.  Forward hop to come later.  Still need to add in the checks for spaces open based on collisions.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,51 +4,80 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    private Vector3 tempPlayerPosition;
-    protected float worldGridSize;  //world grid, tile size in unity units
-    protected float playerMovementSpeed;  //player movement speed in units per second
-    private float internalPlayerSpeed;
-    private float internalAxisValueHorizontal;
-    private float internalAxisValueVertical;
+    private Vector3 playerStartPosition;
 
+    private Vector3 tempStartMarker;
+    private Vector3 tempEndMarker;  //movement stuff
+    private float tempStartTime;
+    private bool movementLock;
+    //private float distCovered;
+    //private float fractionOfJourney;
+
+    protected float worldGridSize;  //world grid, tile size in unity units
+    protected float playerMovementSpeed;  //player movement speed in world grids per second
+    private float internalPlayerSpeed;  //player movement speed in units per seconds
+    private int backStepCount;
+    
     void Start()
     {
-        bool temp1 = SetWorldGridSide(4.0f);
-        bool temp2 = SetPlayerMovementSpeed(1.0f);
+        bool temp1 = SetWorldGridSide(1.5f);
+        bool temp2 = SetPlayerMovementSpeed(3.5f);
         CalculateActualMoveSpeed();
-        tempPlayerPosition = this.gameObject.transform.position;
+
+        playerStartPosition = this.gameObject.transform.position;
+
+        tempEndMarker = new Vector3(0.0f, 0.0f, 0.0f);
+        tempStartTime = Time.time;
+        movementLock = false;
+
+        backStepCount = 0;
     }
 
     void Update()
     {
-        tempPlayerPosition = this.gameObject.transform.position;
-        internalAxisValueHorizontal = Input.GetAxis("Horizontal");
-        internalAxisValueVertical = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown("up") && !movementLock)
+        {
+            tempEndMarker.z += worldGridSize;
+            tempStartTime = Time.time;
+            movementLock = true;
+        }
+        if (Input.GetKeyDown("down") && !movementLock)
+        {
+            tempEndMarker.z -= worldGridSize;
+            tempStartTime = Time.time;
+            movementLock = true;
+        }
+        if (Input.GetKeyDown("left") && !movementLock)
+        {
+            tempEndMarker.x -= worldGridSize;
+            tempStartTime = Time.time;
+            movementLock = true;
+        }
+        if (Input.GetKeyDown("right") && !movementLock)
+        {
+            tempEndMarker.x += worldGridSize;
+            tempStartTime = Time.time;
+            movementLock = true;
+        }
 
-        if (internalAxisValueVertical > 0)
+        if (movementLock)
         {
-            tempPlayerPosition.z += internalPlayerSpeed * Time.deltaTime;
-            this.gameObject.transform.position = tempPlayerPosition;
+            float distCovered = (Time.time - tempStartTime) * internalPlayerSpeed;
+            float fractionOfJourney = distCovered / worldGridSize;
+            this.gameObject.transform.position = Vector3.Lerp(tempStartMarker, tempEndMarker, fractionOfJourney);
+            if (transform.position == tempEndMarker)
+            {
+                movementLock = !movementLock;
+                tempStartMarker = this.gameObject.transform.position;
+                tempEndMarker = tempStartMarker;
+            }
         }
-        if (internalAxisValueVertical < 0)
-        {
-            tempPlayerPosition.z -= internalPlayerSpeed * Time.deltaTime;
-            this.gameObject.transform.position = tempPlayerPosition;
-        }
-        if (internalAxisValueHorizontal < 0)
-        {
-            tempPlayerPosition.x -= internalPlayerSpeed * Time.deltaTime;
-            this.gameObject.transform.position = tempPlayerPosition;
-        }
-        if (internalAxisValueHorizontal > 0)
-        {
-            tempPlayerPosition.x += internalPlayerSpeed * Time.deltaTime;
-            this.gameObject.transform.position = tempPlayerPosition;
-        }
+
+        
     }
 
     public bool SetWorldGridSide(float gridSize)  //Set the worldgrid dimensions, must be a
-    {                                             //positive number, else will default to 1.0f
+    {                                             //positive number, else will default to 1.5f
         if (gridSize > 0.0f)                      //and return false.
         {
             worldGridSize = gridSize;
@@ -56,7 +85,7 @@ public class PlayerScript : MonoBehaviour
         }
         else
         {
-            worldGridSize = 1.0f;
+            worldGridSize = 1.5f;
             return false;
         }
     }
@@ -77,11 +106,59 @@ public class PlayerScript : MonoBehaviour
 
     private void CalculateActualMoveSpeed()
     {
-        internalPlayerSpeed = playerMovementSpeed * worldGridSize;  //adjusts movement speed based on
+        internalPlayerSpeed = worldGridSize * playerMovementSpeed;  //adjusts movement speed based on
     }                                                               //tile size.
-
+    
     private float GetPlayerActualMoveSpeed()
     {
         return internalPlayerSpeed;
+    }
+    
+   public void StepSide(bool clear)
+    {
+        if (clear)
+        {
+            //move side
+        }
+        else
+        {
+            //game over
+            //do score stuff
+        }
+    }
+
+    public void StepForward(bool clear)
+    {
+        if (clear)
+        {
+            //move forward
+        }
+        else
+        {
+            //game over
+            //do score stuff
+        }
+    }
+
+    public void StepBack(bool clear)
+    {
+        if (clear)
+        {
+            backStepCount++;
+            if (backStepCount >= 3)
+            {
+                //game over
+                //do score stuff
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    private void ResetPosition()
+    {
+        this.gameObject.transform.position = playerStartPosition;
     }
 }
