dd6dd59087106e3421850f42c48d7406d5c4c93d
atjones1 <55410868+atjones1@users.noreply.github.com>
2019-10-02 16:29:35 -0400

    Starting Obstacle Generation
    Only trees right now, will add rocks later.

--- Assets/Scripts/GroundGenerator.cs
+++ Assets/Scripts/GroundGenerator.cs
@@ -8,7 +8,8 @@ public class GroundGenerator : MonoBehaviour
     public GameObject groundParent;
 
     private int groundsToGenerate = 20;
-    private int groundPosition = 6;
+
+    private float groundPosition = 9;
 
     private bool useLightGrass = true;
 
@@ -41,20 +42,19 @@ public class GroundGenerator : MonoBehaviour
         }
 
         Instantiate(tempGO1, new Vector3(tempGO1.transform.position.x, tempGO1.transform.position.y, groundPosition), tempGO1.transform.rotation, groundParent.transform);
-        groundPosition++;
+        groundPosition += 1.5F;
 
-        //tempGO1 == groundRoadFirst
         if(tempGO1 == grounds[3])
         {
             GameObject tempGO2 = grounds[Random.Range(4, 6)];
             Instantiate(tempGO2, new Vector3(tempGO2.transform.position.x, tempGO2.transform.position.y, groundPosition), tempGO2.transform.rotation, groundParent.transform);
-            groundPosition++;
+            groundPosition += 1.5F;
 
             if(tempGO2 == grounds[4])
             {
                 GameObject tempGO3 = grounds[5];
                 Instantiate(tempGO3, new Vector3(tempGO3.transform.position.x, tempGO3.transform.position.y, groundPosition), tempGO3.transform.rotation, groundParent.transform);
-                groundPosition++;
+                groundPosition += 1.5F;
                 GameObject tempGO4 = grounds[Random.Range(0, grounds.Length - 3)];
 
                 if(tempGO4 == grounds[0] || tempGO4 == grounds[1])
@@ -73,7 +73,7 @@ public class GroundGenerator : MonoBehaviour
                 }
 
                 Instantiate(tempGO4, new Vector3(tempGO4.transform.position.x, tempGO4.transform.position.y, groundPosition), tempGO4.transform.rotation, groundParent.transform);
-                groundPosition++;
+                groundPosition += 1.5F;
             }
 
             if(tempGO2 == grounds[5])
@@ -96,7 +96,7 @@ public class GroundGenerator : MonoBehaviour
                 }
 
                 Instantiate(tempGO5, new Vector3(tempGO5.transform.position.x, tempGO5.transform.position.y, groundPosition), tempGO5.transform.rotation, groundParent.transform);
-                groundPosition++;
+                groundPosition += 1.5F;
             }
         }
     }
