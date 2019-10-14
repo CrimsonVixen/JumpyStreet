e405cf660f3c328bf296c4cc2d9bad8463f555de
atjones1 <55410868+atjones1@users.noreply.github.com>
2019-10-14 15:27:31 -0400

    Started Traffic Generation
    Started working on the spawning of the cars and trucks, still needs a lot of work.

---
+++ Assets/Scripts/TrafficGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficGenerator : MonoBehaviour
+{
+    public GameObject car;
+
+    public void Start()
+    {
+        //StartCoroutine(GenerateTraffic());
+    }
+
+    private IEnumerator GenerateTraffic()
+    {
+        while(true)
+        {
+            int xPos;
+            float randomValue = Random.value;
+            GameObject tempGO = Instantiate(car, transform.parent, true);
+
+            if(randomValue < 0.5)
+            {
+                xPos = 19;
+                tempGO.transform.localRotation = Quaternion.Euler(0, 0, -90);
+            }
+
+            else
+            {
+                xPos = -19;
+            }
+
+            tempGO.transform.position = new Vector3(xPos, tempGO.transform.position.y, transform.parent.position.z);
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
